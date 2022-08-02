d97044c63483428e5ea8d2538c8e8b78f6fe31e4
BerkleyDEV <Berkley@tekwareenterprises.com>
2022-08-01 19:51:17 -0600

    finding random web scoket games working

--- server.js
+++ server.js
@@ -21,13 +21,62 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  console.log("connection!!!!!!", socket.id);
+  console.log("connection!!!!!!", io.sockets.adapter.rooms);
   socket.emit("connection made", () => {
     console.log("sent connection made");
   });
+  socket.on("join room", (username) => {
+    console.log("join room", username);
+
+    const roomArray = Array.from(io.sockets.adapter.rooms);
+    console.log(
+      "🚀 ~ file: server.js ~ line 35 ~ socket.on ~ roomArray",
+      roomArray
+    );
+    let anyGamesBeingPlayed = false;
+    let anyGamesOpen = false;
+    let userJoinedGame = false;
+    for (let rm of io.sockets.adapter.rooms) {
+      if (typeof rm[0] !== "string") {
+        console.log("NOT STRING");
+        continue;
+      }
+      let isActualGame = rm[0].substring(0, 5) === "game:";
+      console.log(
+        "🚀 ~ file: server.js ~ line 43 ~ socket.on ~ isActualGame",
+        isActualGame
+      );
+      if (isActualGame) {
+        anyGamesBeingPlayed = true;
+        if (rm[1].size === 1) {
+          console.log("set size of 1");
+          socket.join(rm);
+          userJoinedGame = true;
+          break;
+        }
+      }
+    }
+    if (!anyGamesBeingPlayed && !userJoinedGame) {
+      let randomNum = Math.floor(Math.random() * 10000);
+      console.log(
+        "🚀 ~ file: server.js ~ line 61 ~ socket.on ~ randomNum",
+        randomNum
+      );
+      socket.join(`game: ${username}${randomNum}`);
+    } else if (!anyGamesOpen && !userJoinedGame) {
+      let randomNum = Math.floor(Math.random() * 10000);
+      console.log(
+        "🚀 ~ file: server.js ~ line 61 ~ socket.on ~ randomNum",
+        randomNum
+      );
+
+      socket.join(`game: ${username}${randomNum}`);
+    }
+    console.log("io.sockets.adapter.rooms", io.sockets.adapter.rooms);
+  });
   socket.on("game start", (data) => {
     console.log("game start");
-    socket.broadcast.emit("game started", data);
+    socket.to(data.room).emit("game started", data);
   });
 
   socket.on("card played", (data) => {
